e669f660ea961928712096adee4f4143d9025cf7
Winzyl Olvido <prowinzil@gmail.com>
2024-12-20 14:34:55 +0800

    basic layout finished

---
+++ FruitFundTracker/wwwroot/common.blocks/main/main__sidebar.js
@@ -0,0 +1,23 @@
+﻿$(function() {
+
+    $('.sidebar__button').on("click", function () {
+
+        if ($('.main__sidebar-nav').is(":hidden")) {
+            let sideBarWidth = $('.main__sidebar-nav').width();
+            $('.main__content').animate({left: sideBarWidth}, 600 );
+        }
+        
+        if ($('.main__sidebar-nav').is(":visible")) {
+            $('.main__content').animate({ left: "0px"}, 600 );
+        }
+
+        $('.main__sidebar-nav').toggle("slow", function () {
+
+            
+        });
+
+        
+    });
+
+});
+
