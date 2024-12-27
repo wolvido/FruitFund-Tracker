c0e688a480fe40a33f6cc3aa033fae8c9995a46d
Winzyl Olvido <prowinzil@gmail.com>
2024-12-27 16:12:38 +0800

    implemented sidebar logic and files minimizer

--- FruitFundTracker/Program.cs
+++ FruitFundTracker/Program.cs
@@ -1,14 +1,34 @@
+using static System.Net.Mime.MediaTypeNames;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddHsts(options =>
+//{
+//    options.Preload = true;
+//    options.IncludeSubDomains = true;
+//    options.MaxAge = TimeSpan.FromDays(365);
+//});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//web optimizer service added
+builder.Services.AddWebOptimizer(pipeline =>
+{
+    pipeline.AddFiles("text/css", "common.blocks/**/*.css", "desktop.blocks/**/*.css", "mobile.blocks/**/*.css");
+    pipeline.AddFiles("text/javascript", "common.blocks/**/*.js", "desktop.blocks/**/*.js", "mobile.blocks/**/*.js");
+
+    pipeline.MinifyJsFiles("text/javascript", "common.blocks/**/*.js", "desktop.blocks/**/*.js", "mobile.blocks/**/*.js");
+
+    pipeline.AddCssBundle("/bundle.css", "common.blocks/**/*.css", "desktop.blocks/**/*.css", "mobile.blocks/**/*.css");
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -16,6 +36,8 @@ if (!app.Environment.IsDevelopment())
 app.MapControllers();
 
 app.UseHttpsRedirection();
+
+app.UseWebOptimizer();
 
 app.UseStaticFiles();
 
--- FruitFundTracker/wwwroot/common.blocks/main/main__sidebar.js
+++ FruitFundTracker/wwwroot/common.blocks/main/main__sidebar.js
@@ -1,23 +1,59 @@
 ﻿$(function() {
 
-    $('.sidebar__button').on("click", function () {
+    let sidebarHiddenManually;
 
+    //sidebar manual retract and expand
+    $('.sidebar__button').on("click", function () {
         if ($('.main__sidebar-nav').is(":hidden")) {
             let sideBarWidth = $('.main__sidebar-nav').width();
-            $('.main__content').animate({left: sideBarWidth}, 600 );
+
+            $('.main__content').animate({ marginLeft: sideBarWidth }, 600);
+            sidebarHiddenManually = false;
         }
         
         if ($('.main__sidebar-nav').is(":visible")) {
-            $('.main__content').animate({ left: "0px"}, 600 );
+            $('.main__content').animate({ marginLeft: "0px" }, 600);
+            sidebarHiddenManually = true;
         }
 
         $('.main__sidebar-nav').toggle("slow", function () {
-
-            
         });
 
-        
     });
+
+    //sidebar auto retract and expand
+    let isRetracted = false;
+  
+    $(window).on("resize", function () {
+
+        let screenWidth = $(window).width();
+
+        if (screenWidth < 900 && !sidebarHiddenManually && !isRetracted) {
+            isRetracted = true;
+            $('.main__content').stop().animate({ marginLeft: 0 }, { duration: 220 , queue: false });
+            $('.main__sidebar-nav').stop().hide("fast");
+
+        }
+
+        if (screenWidth > 900 && isRetracted && !sidebarHiddenManually) {
+            isRetracted = false;
+
+            let sideBarWidth = $('.main__sidebar-nav').width();
+
+            $('.main__content').animate({ marginLeft: sideBarWidth }, { duration: 220, queue: false });
+            $('.main__sidebar-nav').stop().show("fast");
+        }
+
+    });
+
+    //auto retract on small screens
+    let screenWidth = $(window).width();
+    if (screenWidth < 900 && !sidebarHiddenManually && !isRetracted) {
+        isRetracted = true;
+        $('.main__content').stop().animate({ marginLeft: 0 }, { duration: 220 , queue: false });
+        $('.main__sidebar-nav').stop().hide("fast");
+    }
+
 
 });
 
